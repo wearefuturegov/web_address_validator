97ec2b4331a990283e238e941f0939fc35de98c9
thickpaddy <mwoods@online.ie>
2013-02-22 16:22:08 +0000

    Basic HTTP URL validation, passing specs

--- lib/web_address_validator.rb
+++ lib/web_address_validator.rb
@@ -1,3 +1,20 @@
+require 'uri'
+
+# TODO: locale file for error messages
+
 class WebAddressValidator < ActiveModel::EachValidator
+
+  def validate_each(record, attr, value)
+    uri = URI.parse(value)
+    if uri.scheme.nil?
+      record.errors.add(attr, "is missing protocol (e.g. http://)")
+    elsif uri.scheme != "http" && uri.scheme != "https"
+      record.errors.add(attr, "specifies unsupported protocol, '#{uri.scheme}'")
+    elsif uri.host.nil?
+      record.errors.add(attr, "is missing host name (e.g. www.google.com)")
+    end
+  rescue URI::InvalidURIError
+    record.errors.add(attr, "is invalid")
+  end
 
 end
--- spec/web_address_validator_spec.rb
+++ spec/web_address_validator_spec.rb
@@ -1,11 +1,53 @@
 require 'spec_helper'
 
+# TODO: test for specific error messages
+
 describe WebAddressValidator do
 
-  subject { TestModel }
+  context "bad uri" do
+    subject { TestModel.new("%") }
+    it { should_not be_valid }
+  end
 
-  it "should require a protocol" do
-    subject.new("foo").should_not be_valid
+  context "no host name" do
+    subject { TestModel.new("http://") }
+    it { should_not be_valid }
+  end
+
+  context "known host" do
+
+    let(:known_host) { "google.com" }
+
+    context "http protocol" do
+      subject { TestModel.new("http://#{known_host}") }
+      it { should be_valid }
+    end
+
+    context "https protocol" do
+      subject { TestModel.new("https://#{known_host}") }
+      it { should be_valid }
+    end
+
+    context "unsupported protocol (e.g. ftp)" do
+      subject { TestModel.new("ftp://#{known_host}") }
+      it { should_not be_valid }
+    end
+
+    context "unknown protocol" do
+      subject { TestModel.new("foo://#{known_host}") }
+      it { should_not be_valid }
+    end
+
+    context "missing protocol" do
+      subject { TestModel.new(known_host) }
+      it { should_not be_valid }
+    end
+
+    context "invalidly formatted protocol element of url" do
+      subject { TestModel.new("http:/#{known_host}") }
+      it { should_not be_valid }
+    end
+
   end
 
 end
